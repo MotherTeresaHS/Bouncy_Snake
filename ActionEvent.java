c021335082ff396e9d641f70e2c8aebe04d5682a
Peterzhu123 <Peter.zhu@ocsbstudent.ca>
2019-01-22 14:42:17 -0500

    Create ActionEvent.java

---
+++ ActionEvent.java
@@ -0,0 +1,17 @@
+// Created by Peter Zhu
+// Created on Jan 2019
+// This is actionEvent
+
+public class ActionEvent {
+	
+	private Object source;
+	
+	public ActionEvent(Object source) {
+		this.source = source;
+	}
+	
+	public Object getSource() {
+		return source;
+	}
+
+}
